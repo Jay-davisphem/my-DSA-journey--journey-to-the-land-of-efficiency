0632247202941adf0302b03c89c50aa4d640d3b9
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-07 08:25:26 +0100

    init count inversions recursive

--- py_file/counting_inverstions.py
+++ py_file/counting_inverstions.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def count_invertions(arr: List[int]) -> int:
-    """'An iterative and brute force approach of counting the number of invertions in an array(usefull in recommendations system"""
+    """'An iterative and brute force(O(n^2)) approach of counting the number of invertions in an array(usefull in recommendations system"""
     n = len(arr)
     n_inv = 0
     for i in range(n):
@@ -12,9 +12,14 @@ def count_invertions(arr: List[int]) -> int:
     return n_inv
 
 
-def count_split(arr):
+def count_split(arr: List[int]):
     """count split of recursive count_inversions"""
     pass
+
+
+def count_inversions(arr: List[int]):
+    """'An recursive and efficient(O(nlogn)) approach of counting the number of invertions in an array(usefull in recommendations system"""
+    ...
 
 
 print(count_invertions([1, 3, 5, 2, 4, 6]))
