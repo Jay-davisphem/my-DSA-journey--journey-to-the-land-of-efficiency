656a4eb9ec3f3a27e8cf746d20cf27b487678e1c
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-11 03:51:58 +0100

    solve challenge 4.4

---
+++ learncpp/ch4.4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+
+int getHeight()
+{
+    int h{};
+    std::cout << "Enter the height of the tower in meters: ";
+    std::cin >> h;
+    return h;
+};
+
+void printResult(int h)
+{
+    std::cout << std::setprecision(1) << std::fixed;
+
+    constexpr float g{9.8};
+    int x_seconds{0};
+    while (x_seconds <= 5)
+    {
+        float g_h{g * (x_seconds * x_seconds) / 2};
+        if (g_h >= h)
+            std::cout << "At " << x_seconds << "s econds, the ball is on the ground\n";
+        else
+            std::cout << "At " << x_seconds << " seconds, the ball is at height: " << h - g_h << " meters\n";
+        ++x_seconds;
+    }
+};
+
+int main()
+{
+
+    int h{};
+    h = getHeight();
+    printResult(h);
+}
