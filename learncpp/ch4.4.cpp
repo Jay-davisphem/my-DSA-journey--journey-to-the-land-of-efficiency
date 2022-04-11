5009ac71b018f5d25081ed5d6cdcef2f61886306
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-11 04:04:26 +0100

    modularize challenge 4.4

--- learncpp/ch4.4.cpp
+++ learncpp/ch4.4.cpp
@@ -9,15 +9,19 @@ int getHeight()
     return h;
 };
 
+float calculateHeight(int x_seconds)
+{
+    constexpr float g{9.8};
+    return g * (x_seconds * x_seconds) / 2;
+}
 void printResult(int h)
 {
     std::cout << std::setprecision(1) << std::fixed;
 
-    constexpr float g{9.8};
     int x_seconds{0};
     while (x_seconds <= 5)
     {
-        float g_h{g * (x_seconds * x_seconds) / 2};
+        float g_h{calculateHeight(x_seconds)};
         if (g_h >= h)
             std::cout << "At " << x_seconds << "seconds, the ball is on the ground\n";
         else
